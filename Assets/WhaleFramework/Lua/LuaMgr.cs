e339d106917a3846bec8c0f20ab9de8c1fef3b22
yuhanwei <6168981@qq.com>
2016-12-06 20:08:53 +0800

    @add simple  luamgr

---
+++ Assets/WhaleFramework/Lua/LuaMgr.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using LuaInterface; 
+
+public class LuaMgr : MonoBehaviour
+{
+
+    private static LuaMgr _instance;
+
+    public static LuaMgr Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                Debug.LogError("LuaMgr is null!!");
+            }
+            return _instance;
+        }
+    }
+
+    LuaState lua = null;
+
+    void Awake()
+    {
+        _instance = this;
+    }
+
+    // Use this for initialization
+    public void Init () {
+        lua = new LuaState();
+        lua.Start();
+        LuaBinder.Bind(lua);
+    }
+
+    public void StartLua()
+    {
+        lua.DoFile("Main.lua");
+        LuaFunction main = lua.GetFunction("Main");
+        main.Call();
+        main.Dispose();
+        main = null;
+    }
+}
