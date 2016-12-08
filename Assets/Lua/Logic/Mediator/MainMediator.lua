908c20ed6ee9cb0b0f649478e33c40d5e4bd9217
yuhanwei <6168981@qq.com>
2016-12-08 20:41:20 +0800

    @

--- Assets/Lua/Logic/Mediator/MainMediator.lua
+++ Assets/Lua/Logic/Mediator/MainMediator.lua
@@ -1,5 +1,15 @@
 MainMediator = class("MainMediator", Mediator)
 
-function MainMediator:initialize()
-	-- body
+function MainMediator:initialize(mediatorName, viewComponent)
+	Mediator.initialize(self, mediatorName, viewComponent)
+end
+
+function MainMediator:ListNotificationInterests()
+	return {}
+end
+
+function MainMediator:HandleNotification(notification)
+	local name = notification:GetName()
+	local body = notification:GetBody()
+	
 end
